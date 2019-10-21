400fd1db63f83d30d24cb84af365ac007b73792f
Gaines <rgaines@uidaho.edu>
2019-10-21 12:19:57 -0700

    Added functional external media backup script

--- BackupScripts/BackupToMedia.ps1
+++ BackupScripts/BackupToMedia.ps1
@@ -1,68 +1,111 @@
 # Back Up User's Files to External Media #
 
 $ErrorActionPreference = "SilentlyContinue"
-function MakeBackupDirectory($target,$origin)
+
+Write-Host -ForegroundColor Yellow "
+------------------------------------
+|    Backup To External Media      |
+|    Author: RWG                   |
+------------------------------------ 
+           "
+
+function MakeBackupDirectory($backupDirectory)
 {
-    Write-Host -ForegroundColor Yellow "[*] Transitioning to backup target $target [*]"
+    Write-Host -ForegroundColor Yellow "[*] Creating the backup directory [*]"
+
     try{
-        Set-Location $target
+        # Get the date and time and then format for use as a timestamp #
 
         $timeStamp = Get-Date -Format o | Foreach-Object{ $_ -replace ":",'.' }
 
+        # Assign time stamped identifier to the backup directory
+
         $backupDirectoryName = "Backup_"+$timeStamp
 
-        $backupPath = $target+$backupDirectory
+        $backupTargetName =  $backupDirectory.DriveLetter+":\"+$backupDirectoryName
 
-        New-Item -ItemType "Directory" -Path $backupPath
+        # Create the backup directory on the external media device #
 
-        Set-Location $origin
+        Write-host -ForegroundColor Yellow "[~] Attempting creation of: ", $backupTargetName
+
+        $bd = New-Item -ItemType "Directory" -Path $backupTargetName
+
+        if($bd)
+        {
+            Write-Host -ForeGroundColor Green "[*] Backup directory created at: ", $backupTargetName
+            #
+            return $backupTargetName
+        }
     }
     catch
     {
-        Write-Host -ForegroundColor Red "[!] Failed to switch directories [!]"
+        Write-Host -ForegroundColor Red "[!] Failed to create directory [!]"
 
-        return
+        exit
     }
 }
 
-Write-Host -ForegroundColor Yellow "[*] Gathering volumes..." ; Start-Sleep(1)
+function main()
+{
+    # Identify all active volumes #
 
-Get-Volume | Format-Table DriveLetter,FileSystem,SizeRemaining
+    Write-Host -ForegroundColor Yellow "[*] Gathering volumes..." ; Start-Sleep -Seconds 1
 
-$targetVolume = Read-Host "[+] Enter the volume drive letter" ; $backupTarget = ""
+    # List the Driv Letter, File System Type, and the total size remaining
 
-try {
-    $backupTarget = Get-Volume -DriveLetter $targetVolume
+    Get-Volume | Format-Table DriveLetter,FileSystem,SizeRemaining
 
-    Write-Host -ForegroundColor Green "[*] Located Volume: ", $backupTarget.DriveLetter 
+    $targetVolume = Read-Host "[+] Enter the volume drive letter" 
 
-    $backupTarget = $backupTarget + ":\"
+    try {
+        $backupTarget = Get-Volume -DriveLetter $targetVolume
+
+        Write-Host -ForegroundColor Green "[*] Located Volume: ", $backupTarget.DriveLetter 
+    }
+    catch {
+        Write-Host -ForegroundColor Red "[!] Failed to locate volume [!]"  
+
+        exit
+    }
+
+    $backupTarget = $backupTarget.DriverLetter.ToString()
+
+    Write-Host -ForegroundColor yellow "[*] Gathering user and directory data [*]" ; Start-Sleep -Seconds 1
+
+    try {
+        $currentUser = $env:USERNAME
+
+        Write-Host -ForegroundColor Green "[*] Located user: ", $currentUser
+    }
+    catch {
+        Write-Host -ForegroundColor Red "[!] Failed to identify user [!]"
+
+        exit
+    }
+
+    $userFileSystemRoot = "C:\Users\$currentUser"
+
+    Write-Host -ForegroundColor Blue "[*] Starting at-> ", $userFileSystemRoot ; Start-Sleep -Seconds 1
+
+    $backupDirectory = MakeBackupDirectory($backupTarget)
+
+    Write-Host -ForegroundColor Yellow "[*] Relocating to the root of the user file system [*]"
+
+    Set-Location $userFileSystemRoot 
+
+    $userDirectories = @(Get-ChildItem $userFileSystemRoot) ; $dirCount = $userDirectories.Length
+
+    for($i = 0; $i -lt $dirCount; $i++)
+    {
+        $absPath = $userFileSystemRoot+'\'+$userDirectories[$i]
+    
+        Write-Progress -Activity "[*] Backing up user files..." -CurrentOperation -Completed -Status "$i% Complete" -PercentComplete $i
+
+        Write-Host -ForegroundColor Blue "[*] Backing up: `t`t", $absPath, "`t`t ==>> `t`t", $backupDirectory
+
+        Copy-Item -Path $absPath -Destination $backupDirectory -Recurse
+    }
+    Write-Host -ForegroundColor "[*] Backup Complete [*]"
 }
-catch {
-    Write-Host -ForegroundColor Red "[!] Failed to locate volume [!]"  
-}
 
-Write-Host -ForegroundColor yellow "[*] Gathering user and directory data [*]" ; Start-Sleep -Seconds 1
-
-try {
-    $currentUser = $env:USERNAME
-
-    Write-Host -ForegroundColor Green "[*] Located user: ", $currentUser
-}
-catch {
-    Write-Host -ForegroundColor Red "[!] Failed to identify user [!]"
-}
-
-$fileSystemRoot = "C:\Users\$currentUser"
-
-Write-Host -ForegroundColor Blue "[*] Starting at-> ", $fileSystemRoot ; Start-Sleep -Seconds 1
-
-MakeBackupDirectory($backupTarget,$fileSystemRoot)
-
-#Set-Location $fileSystemRoot 
-
-#Get-ChildItem -Force -Recurse | Foreach-Object{Write-Host $_.FullName ; Start-Sleep -Seconds 1}
-
-
-
-
+main
