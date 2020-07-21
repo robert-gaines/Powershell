7f17f93e9ad05f67db84174f86c1a0601b33a464
Robert W. Gaines <robert.gaines@homelab.winlab.local>
2020-07-20 20:07:11 -0700

    Added Rick Roll scripts

---
+++ Administration/RickRoll.ps1
@@ -0,0 +1,18 @@
+﻿
+function RickRoll()
+{
+    try
+    {
+        start microsoft-edge:https://www.youtube.com/watch?v=dQw4w9WgXcQ
+    }
+    catch
+    {
+        Write-Host -ForegroundColor Red "[!] Failed to open web browser "
+
+        Start-Sleep -Seconds 1
+
+        exit
+    }
+}
+
+RickRoll
