27888a00891c1a5f55dcbda831d32fe7660f6f26
Robert Gaines <robert.gaines@hushmail.com>
2020-07-29 15:53:07 -0700

    Added the mouse mover scheduled task
    Use in conjunction with MouseMover.exe

---
+++ GeneralUtility/Mouse-Mover.ps1
@@ -0,0 +1,19 @@
+﻿
+try
+{
+    Copy-Item -Path MouseMover.exe -Destination C:\Temp -Force
+}
+catch
+{
+    exi
+}
+
+
+
+$action = New-ScheduledTaskAction -Execute "C:\Temp\MouseMover.exe" 
+
+$user = $env:USERNAME
+
+$trigger = New-ScheduledTaskTrigger -AtLogOn
+
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "MoveMouse" -User $user -Description "Move the mouse!" -RunLevel Highest -Force
