27579dfb8b9707836acb0549680025f41120163d
Robert W. Gaines <robert.gaines@internal.subterfuge.biz>
2023-03-02 14:00:11 -0800

    Routine

---
+++ Logs/LogsToHTML.ps1
@@ -0,0 +1,7 @@
+﻿$computerName = $env:COMPUTERNAME
+$timeStamp    = Get-Date -Format hhmmss_MMddyyyy
+$fileName     = $computerName+"_event_viewer_logs_"+$timeStamp+".html"
+
+Get-EventLog -LogName * | Foreach-Object {
+                                            Get-EventLog -LogName $_.Log | ConvertTo-Html | Out-File -Append $fileName
+                                         }
---
+++ Software/SoftwareInventory.ps1
@@ -0,0 +1,3 @@
+﻿$computerName = $env:COMPUTERNAME
+$fileName     = $computerName+"_software.html"
+Get-WmiObject -Class Win32_Product | Select-Object -Property Name, Version, Vendor | ConvertTo-Html | Out-File $fileName
